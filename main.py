5b1dc044f6443de2c09b823f67695a00cb37126d
nicoapallaz <nicholasflores2006@gmail.com>
2025-08-31 18:15:36 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+question1 = 5 * 6
+question2 = 8 / 3
+question3 = 8 // 3
+question4 = 8 % 3
+question5 = 4200000 + 23090
+question6 = 4200000 - 7633
+
+print("The answer to your query is " + str(question1))
+print("The answer to your query is " + str(question2))
+print("The answer to your query is " + str(question3))
+print("The answer to your query is " + str(question4))
+print("The answer to your query is " + str(question5))
+print("The answer to your query is " + str(question6))
+
+print("Nicholas Flores")
